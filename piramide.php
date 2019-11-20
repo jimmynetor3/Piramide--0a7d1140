99b8f0869fedc926933f34448459cee0a7dae470
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2019-11-20 09:30:10 +0100

    uitwerking van opdracht

--- piramide.php
+++ piramide.php
@@ -1,1 +1,10 @@
-# Piramide--0a7d1140
+# Piramide--0a7d1140 #
+
+<?php
+$number = readline("hoeveel wil je ");
+
+for ($i = 1; $i < $number ; $i++) {
+    echo str_repeat("*" , $i);
+    echo PHP_EOL;
+}
+?>
